d09acc1d88142c494337f49b364804ee1c4419e4
sid2021 <wolanski.jakub@gmail.com>
2020-09-05 18:50:12 +0200

    Updates

---
+++ precache-manifest.d983d126d94548c3b3f8608d9ce2bb42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b199ec8de70f48e6b20f5c7258efaaa",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "809b8aef3894df8060d3",
+    "url": "/Pomodoro-Clock/static/css/main.482cea87.chunk.css"
+  },
+  {
+    "revision": "7bbc3f30b12cd3e3963d",
+    "url": "/Pomodoro-Clock/static/js/2.c20148b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.c20148b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809b8aef3894df8060d3",
+    "url": "/Pomodoro-Clock/static/js/main.e4b05588.chunk.js"
+  },
+  {
+    "revision": "2e87c2954f9d3212e561",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.2bbcdaee.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Pomodoro-Clock/precache-manifest.d983d126d94548c3b3f8608d9ce2bb42.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Pomodoro-Clock/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
